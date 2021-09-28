36399bc3f354c6b8ba74abc7e0e66e75a8e6a177
rapsealk <piono623@naver.com>
2021-09-28 16:13:50 +0900

    Fix docker gpu allocation option

--- docker/pytorch/run.sh
+++ docker/pytorch/run.sh
@@ -1,1 +1,2 @@
-docker run --gpus 0 -it --rm -p 10020:8888 -v ${PWD}:/workspace nvcr.io/nvidia/pytorch:21.08-py
+docker run --gpus '"device=0"' -it --rm -p 10020:8888 -v ${PWD}:/workspace nvcr.io/nvidia/pytorch:21.08-py
+# docker run --gpus 0 -it --rm -p 10020:8888 -v ${PWD}:/workspace nvcr.io/nvidia/pytorch:21.08-py
